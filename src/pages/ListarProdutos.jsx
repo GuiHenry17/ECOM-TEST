a7db2bebd6fddd42735cdd926f80b8c7a74e528a
GuiHenry17 <gui201629@gmail.com>
2024-10-28 17:39:15 -0400

    Começo CSS Modules

--- src/pages/ListarProdutos.jsx
+++ src/pages/ListarProdutos.jsx
@@ -1,14 +1,18 @@
+import styles from "../styles/listarProdutos.module.css"
+
 export default function ListaProdutos({lista}){
     return (
-        <>
+        <div className={styles.container}>
             {lista.map(produto => (
-                <div key={produto.id}>
+                <div key={produto.id} className={styles.produtos}>
+                    <img src={produto.image} alt={produto.title} width={200} />
                     <h2>{produto.title}</h2>
                     <p>{produto.description}</p>
-                    <p>Preço: {produto.price}</p>
-                    <img src={produto.image} alt={produto.title} width={200} />
+                    <h4>{produto.price}</h4>
+                    <button>COMPRAR</button>
                 </div>
             ))}
-        </>
+
+        </div>
     )
 }
