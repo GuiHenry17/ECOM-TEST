1070e00e575ed9eb972a562e7f3e8bd63dd03687
GuiHenry17 <gui201629@gmail.com>
2024-11-25 12:51:43 -0300

    Implementação dos Filtros *def*

--- src/components/Buscar.jsx
+++ src/components/Buscar.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import styles from "../styles/filters.module.css"  
 
 
 export default function Buscar({lista, setLista}) {
 
+  const [originalLista, setOriginalLista] = useState([])
+  const [input, setInput] = useState("")
+
+    useEffect(() => {
+      if (lista.length > 0 && originalLista.length === 0) {
+        setOriginalLista(lista)
+      }
+    }, [lista, originalLista.length])
+
+
+    useEffect(() => {
+      if (input.trim() === "") {
+        setLista([...originalLista])
+      } else {
+        const listaFiltrada = originalLista.filter((produto) =>
+         produto.title.toLowerCase().includes(input.toLowerCase())
+        )
+        setLista(listaFiltrada)
+      }
+    }, [input, originalLista, setLista])
+    
 
     const orderAz = () => {
         const listaOrdenada = [...lista].sort((a,b) => a.title.localeCompare(b.title))
@@ -25,18 +46,21 @@ export default function Buscar({lista, setLista}) {
         const listaOrdenada = [...lista].sort((a,b) => b.price - a.price)
         setLista(listaOrdenada)
       }
-    
-    
-
 
   return (
-    
     <div className={styles.filters}>
+      <h1>Filtros</h1>
+      <div className={styles.buttonsContainer}>
       <button className={styles.buttons} onClick={() => orderAz()}>A-Z</button>
       <button className={styles.buttons} onClick={() => orderZa()}>Z-A</button>
-      <button className={styles.buttons} onClick={() => orderLowerPrice()}>LowerPrice</button>
-      <button className={styles.buttons} onClick={() => orderHigherPrice()}>HigherPrice</button>
+      <button className={styles.buttons} onClick={() => orderLowerPrice()}>Lower Price</button>
+      <button className={styles.buttons} onClick={() => orderHigherPrice()}>Higher Price</button>
+      </div>
+      <input
+      id="busca-input"
+      placeholder="Informe o nome do produto"
+      onChange={(event) => setInput(event.target.value)}
+      />
     </div>
-    
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"  
 import { ToastContainer, toast } from "react-toastify"  
 import "react-toastify/dist/ReactToastify.css"  
-import Loading from "../components/Loading"
 import Buscar from "../components/Buscar"
 
 export default function Home() {
